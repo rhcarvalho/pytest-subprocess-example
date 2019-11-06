f13dfc99f3ee334aa8ab41108bb3738e2df13775
Rodolfo Carvalho <rhcarvalho@gmail.com>
2019-11-07 00:44:36 +0100

    Add interactive layer to reusable_program.py

--- reusable_program.py
+++ reusable_program.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def greet_100(name, age):
     try:
         age = int(age)
@@ -5,3 +8,17 @@ def greet_100(name, age):
         raise ValueError("Sorry, I could not understand your age!")
     year = str((2014 - age)+100)
     return name + " will be 100 years old in the year " + year
+
+
+def main():
+    name = input("What is your name: ")
+    age = input("How old are you: ")
+    try:
+        print(greet_100(name, age))
+    except ValueError as exception:
+        print(exception, file=sys.stderr)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
