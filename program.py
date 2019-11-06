54f1e622700d131b401f044d8e9dec98be9afcec
Rodolfo Carvalho <rhcarvalho@gmail.com>
2019-11-06 23:30:05 +0100

    Handle invalid input for age
    This commit makes the existing tests pass.

--- program.py
+++ program.py
@@ -1,4 +1,11 @@
+import sys
+
+
 name = input("What is your name: ")
-age = int(input("How old are you: "))
+try:
+    age = int(input("How old are you: "))
+except ValueError:
+    print("Sorry, I could not understand your age!", file=sys.stderr)
+    sys.exit(1)
 year = str((2014 - age)+100)
 print(name + " will be 100 years old in the year " + year)
