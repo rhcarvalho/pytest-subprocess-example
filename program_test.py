5b71faa5902d8288848b620eb7e6811d021e84fb
Rodolfo Carvalho <rhcarvalho@gmail.com>
2019-11-06 23:04:34 +0100

    Add falling test case

---
+++ program_test.py
@@ -0,0 +1,26 @@
+import os.path
+import subprocess
+import sys
+
+
+def test_program():
+    completed_process = run_program(b"Rodolfo\n42\n")
+    got = {
+        "returncode": completed_process.returncode,
+        "stdout": completed_process.stdout,
+        "stderr": completed_process.stderr,
+    }
+    want = {
+        "returncode": 0,
+        "stdout": b"Rodolfo will be 100 years old in the year 2020",
+        "stderr": b"",
+    }
+    assert got == want
+
+
+def run_program(input):
+    return subprocess.run(
+        [sys.executable, os.path.join(os.path.dirname(__file__), "program.py")],
+        input=input,
+        capture_output=True,
+    )
